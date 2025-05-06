71fd5d026665d40bd9fec00054f8bc23dcae81a6
harshava123 <harshav123.mru@gmail.com>
2025-05-06 15:27:11 +0530

    finals

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
     <footer className="w-full bg-black pt-16">
       <div className="w-full mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12">
-          {/* Company Info */}
+          
           <div className="space-y-6">
             <div className="w-16 h-16">
               <img src={bgimg} alt="Company Logo" className="w-full h-full object-cover" />
